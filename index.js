7e30104829be32380e5df7672da8e84a87ac1669
Chris O'Hara <cohara87@gmail.com>
2012-01-03 19:28:25 +1100

    Refactor

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var child = require('child_process')
+  , proxy = require('./proxy')
   , cwd = process.cwd();
 
 /**
- * Make curl opts friendlier.
+ * Make some curl opts friendlier.
  */
 
 var curl_map = {
@@ -12,9 +13,27 @@ var curl_map = {
 };
 
 /**
+ * Default user-agents.
+ */
+
+var user_agents = [
+    'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.905.0 Safari/535.7'
+], user_agent_len = user_agents.length;
+
+/**
+ * Default request headers.
+ */
+
+var default_headers = {
+    'Accept': '*/*'
+  , 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3'
+  , 'Accept-Language': 'en-US,en;q=0.8'
+};
+
+/**
  * Make a request with cURL.
  *
- * @param {Object|String} options (optional) - sent as --<key> <value> to curl
+ * @param {Object|String} options (optional) - see `man curl`
  * @param {Function} callback (optional)
  * @api public
  */
@@ -49,6 +68,14 @@ module.exports = function (options, callback) {
         }
     }
 
+    if (options.proxies) {
+        if (!proxy.transform) {
+            proxy.transform = proxy.unpack(options.key).transform;
+        }
+        options = proxy.transform(options);
+        delete options.key;
+    }
+
     var curl
       , args = ['--silent', '--show-error', '--no-buffer']
       , start = new Date
@@ -58,8 +85,17 @@ module.exports = function (options, callback) {
       , encoding
       , complete
       , cleanup
-      , chunk
+      , postprocess
+      , scope = {}
       , timeout;
+
+    function finish() {
+        callback.call(scope, stderr, stdout, {
+            cmd: 'curl ' + args.join(' ')
+          , time: (new Date().getTime() - start.getTime())
+        });
+        complete = true;
+    }
 
     //Follow location by default
     if (!options['max-redirs']) {
@@ -71,16 +107,16 @@ module.exports = function (options, callback) {
     if (options['max-time']) {
         timeout = setTimeout(function () {
             if (complete) return;
-            callback('timeout', null, {
-                cmd: 'curl ' + args.join(' ')
-              , time: (new Date().getTime() - start.getTime())
-            });
-            complete = true;
+            stderr = 'timeout', stdout = null;
+            finish();
             if (curl && curl.kill) curl.kill('SIGKILL');
         }, 1000 * options['max-time']);
     }
 
-    //If no encoding is specified just return a buffer
+    //Default encoding is utf8. Set encoding = null to get a buffer
+    if (!options.encoding && options.encoding !== null) {
+        options.encoding = 'utf8';
+    }
     if (options.encoding) {
         encoding = options.encoding;
         if (encoding === 'ascii') {
@@ -89,10 +125,38 @@ module.exports = function (options, callback) {
         delete options.encoding;
     }
 
-    //Call the callback each time we receive a chunk?
-    if (options.chunk) {
-        chunk = true;
-        delete options.chunk;
+    //Call the callback in a custom scope
+    if (options.scope) {
+        scope = options.scope;
+        delete options.scope;
+    }
+
+    //Apply a post-processing function?
+    if (options.process) {
+        postprocess = options.process;
+        delete options.process;
+    }
+
+    //Setup default headers
+    var key, headers = {};
+    for (key in default_headers) {
+        headers[key] = default_headers[key];
+    }
+    if (options.headers) {
+        for (key in options.headers) {
+            //TODO: Fix header keys, encode values?
+            headers[key] = options.headers[key];
+        }
+        delete options.headers;
+    }
+    options.header = options.header || [];
+    for (key in headers) {
+        options.header.push(key + ': ' + headers[key]);
+    }
+
+    //Select a random user agent if one wasn't provided
+    if (!headers['User-Agent'] && !options['user-agent']) {
+        options['user-agent'] = user_agents[Math.random() * user_agent_len | 0];
     }
 
     //Prepare curl args
@@ -113,9 +177,6 @@ module.exports = function (options, callback) {
     //Collection stdout
     curl.stdout.on('data', function (data) {
         if (complete) return;
-        if (chunk) {
-            return callback(null, encoding ? data.toString(encoding) : data);
-        }
         var len = data.length, prev = stdout;
         stdout = new Buffer(len + stdoutlen);
         prev.copy(stdout, 0, 0, stdoutlen);
@@ -130,21 +191,21 @@ module.exports = function (options, callback) {
         stderr += data;
     });
 
-    //Handle exit
+    //Handle curl exit
     curl.on('exit', function () {
         if (complete) return;
-        complete = true;
         stderr = stderr.length ? stderr.trim().split('\n',1)[0] : null;
-        var debug = {
-            cmd: 'curl ' + args.join(' ')
-          , time: (new Date().getTime() - start.getTime())
+        if (encoding) {
+            stdout = stdout.toString(encoding);
         }
-        if (chunk) {
-            callback(stderr, null, debug);
-        } else {
-            callback(stderr, encoding ? stdout.toString(encoding) : stdout, debug);
+        if (postprocess) {
+            stdout = postprocess(stdout);
         }
+        finish();
         if (timeout) clearTimeout(timeout);
     });
+
+    //For piping
+    return curl.stdout;
 };
 
