8749e6994f252f6782d8b3fec71181ec71a6404c
Chris O'Hara <cohara87@gmail.com>
2012-01-04 17:52:07 +1100

    Read useragents from a file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var child = require('child_process')
   , util = require('util')
+  , fs = require('fs')
   , proxy = require('./proxy')
   , cwd = process.cwd();
 
@@ -10,27 +11,15 @@ var child = require('child_process')
 var curl_map = {
     timeout: 'max-time'
   , redirects: 'max-redirs'
+  , useragent: 'user-agent'
 };
 
 /**
  * Default user-agents.
  */
 
-var user_agents = [
-    'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.905.0 Safari/535.7'
-  , 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'
-  , 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)'
-  , 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 6.0)'
-  , 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13'
-  , 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6'
-  , 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)'
-  , 'Opera/9.20 (Windows NT 6.0; U; en)'
-  , 'Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2) Gecko/20100115 Firefox/3.6'
-  , 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; MS-RTC LM 8)'
-  , 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.2 (KHTML, like Gecko) Chrome/6.0'
-  , 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_7; en-us) AppleWebKit/533.4 (KHTML, like Gecko) Version/4.1 Safari/533.4'
-  , 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_6) AppleWebKit/534.22 (KHTML, like Gecko) Chrome/11.0.683.0 Safari/534.22'
-], user_agent_len = user_agents.length;
+var user_agents = fs.readFileSync('useragents.txt').toString().split('\n').slice(0, -1)
+  , user_agent_len = user_agents.length;
 
 /**
  * Default request headers.
